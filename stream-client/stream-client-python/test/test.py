704c4699a168c1bdc85f31dc759657438931257d
Constantine Povietkin <kpovetkin@continuuity.com>
2014-08-22 09:58:09 +0300

    [CYB-4] Directory structure changes

--- stream-client/stream-client-python/test/test.py
+++ stream-client/stream-client-python/test/test.py
@@ -2,6 +2,12 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
+
 from serviceconnector import NoFoundException
 from streamwriter import StreamWriter
 from streamclient import *
