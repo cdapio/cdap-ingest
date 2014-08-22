76dfd197cd92ad567f59f07ea2a78c4452c2ad8d
Alina Makogon <amakogon@cybervisiontech.com>
2014-08-22 20:51:57 +0300

    CYB-8: Create File DropZone Polling Service

---
+++ file-drop-zone/src/main/java/co/cask/cdap/file/dropzone/package-info.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2014 Cask, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+/**
+<<<<<<< HEAD:file-drop-zone/src/main/java/co/cask/cdap/file/dropzone/package-info.java
+ * This package contains classes for File DropZone Java Application.
+ * File DropZone is to support user easily perform bulk ingestion using local file.
+=======
+ * This package contains internal classes for the StreamClient API.
+>>>>>>> origin/master:stream-client/stream-client-java/src/main/java/co/cask/cdap/client/package-info.java
+ */
+package co.cask.cdap.file.dropzone;
