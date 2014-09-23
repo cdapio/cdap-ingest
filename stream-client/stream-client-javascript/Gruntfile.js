223d466fdee784af836fa1e4db035b64d14430c7
Constantine Povietkin <kpovetkin@continuuity.com>
2014-09-23 14:44:43 +0300

    [cyb-3] build system initial

---
+++ stream-client/stream-client-javascript/Gruntfile.js
@@ -0,0 +1,57 @@
+module.exports = function (grunt) {
+    'use strict';
+    // Project configuration
+    grunt.initConfig({
+        // Metadata
+        pkg: grunt.file.readJSON('package.json'),
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+            '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+            '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+            '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+            ' Licensed <%= props.license %> */\n',
+        // Task configuration
+        concat: {
+            options: {
+                banner: '<%= banner %>',
+                stripBanners: true
+            },
+            browser_dist: {
+                src: ['src/*.js'],
+                dest: 'dist/browser/cask-client.js'
+            },
+            node_dist: {
+                src: ['src/*.js'],
+                dest: 'dist/nodejs/s'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '<%= banner %>'
+            },
+            dist: {
+                src: '<%= concat.browser_dist.dest %>',
+                dest: 'dist/broser/cask-client.min.js'
+            }
+        },
+        watch: {
+            gruntfile: {
+                files: '<%= jshint.gruntfile.src %>',
+                tasks: ['jshint:gruntfile']
+            },
+            lib_test: {
+                files: '<%= jshint.lib_test.src %>',
+                tasks: ['jshint:lib_test', 'nodeunit']
+            }
+        }
+    });
+
+    // These plugins provide necessary tasks
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-mocha');
+
+    // Default task
+    grunt.registerTask('default', ['concat', 'uglify']);
+};
+
