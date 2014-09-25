11ccfaf951794e4b683948e722364bb940e26481
Ali Anwar <anwar1@berkeley.edu>
2014-09-25 02:12:01 -0700

    fix tests, all is good.

--- cdap-stream-clients/python/streamclient/v2/test/stream_test_base.py
+++ cdap-stream-clients/python/streamclient/v2/test/stream_test_base.py
@@ -66,105 +66,26 @@ class StreamTestBase(object):
         authClient.configure(authConfig)
 
         self.config.set_auth_client(authClient)
-
         self.sc = StreamClient(self.config)
 
-        self.__BASE_URL = u'http://{0}:{1}/v2'.format(
-            self.config.host, self.config.port)
-        self.__REQUEST_PLACEHOLDERS = {
-            u'streamid': u'<streamid>'
-        }
-        self.__REQUESTS = {u'base_stream_path': self.__BASE_URL + u'/streams'}
-        self.__REQUESTS[u'stream'] = u'{0}/{1}'.format(
-            self.__REQUESTS[u'base_stream_path'],
-            self.__REQUEST_PLACEHOLDERS[u'streamid'])
-        self.__REQUESTS[u'consumerid'] = u'{0}/{1}'.format(
-            self.__REQUESTS[u'stream'], u'consumer-id')
-        self.__REQUESTS[u'dequeue'] = u'{0}/{1}'.format(
-            self.__REQUESTS[u'stream'], u'dequeue')
-        self.__REQUESTS[u'config'] = u'{0}/{1}'.format(
-            self.__REQUESTS[u'stream'], u'config')
-        self.__REQUESTS[u'info'] = u'{0}/{1}'.format(
-            self.__REQUESTS[u'stream'], u'info')
-        self.__REQUESTS[u'truncate'] = u'{0}/{1}'.format(
-            self.__REQUESTS[u'stream'], u'truncate')
-
     def test_reactor_successful_connection(self):
-        try:
-            self.sc.create(self.validStream)
-        except:
-            self.fail(u'Reactor connection failed')
+        # Create stream
+        self.sc.create(self.validStream)
 
-    def test_reactor_failure_connection(self):
-        url = self.__REQUESTS[u'stream'].replace(
-            self.__REQUEST_PLACEHOLDERS[u'streamid'],
-            self.validStream
-        )
-
-        url = url.replace(u'{0}'.format(self.config.port), u'0')
-
-        self.assertRaises(
-            Exception,
-            requests.get,
-            url
-            )
-
-    def test_create(self):
-        url = self.__REQUESTS[u'stream'].replace(
-            self.__REQUEST_PLACEHOLDERS[u'streamid'],
-            self.validStream
-        )
-
-        response = requests.put(url)
-
-        self.assertEqual(response.status_code, 200)
-
-    def test_set_ttl_valid_stream(self):
+        # Set ttl
         ttl = 88888
+        self.sc.set_ttl(self.validStream, ttl)
 
-        try:
-            self.sc.set_ttl(self.validStream, ttl)
-        except NotFoundError:
-            self.fail(u'StreamClient.set_ttl() failed')
+        #Get ttl and verify
+        self.sc.get_ttl(self.validStream)
 
-    def test_set_ttl_invalid_stream(self):
-        ttl = 88888
-
-        self.assertRaises(
-            NotFoundError,
-            self.sc.set_ttl,
-            self.invalidStream,
-            ttl
-        )
-
-    def test_get_ttl_valid_stream(self):
-        try:
-            self.sc.get_ttl(self.validStream)
-        except NotFoundError:
-            self.fail(u'StreamClient.getTTL() failed')
-
-    def test_get_ttl_invalid_stream(self):
-        self.assertRaises(
-            NotFoundError,
-            self.sc.get_ttl,
-            self.invalidStream
-        )
-
-    def test_create_writer_successful(self):
+        # Succesfully create a writer for the stream
         self.assertIsInstance(
             self.sc.create_writer(self.validStream),
             StreamWriter
         )
 
-    def test_create_writer_invalid_stream(self):
-
-        self.assertRaises(
-            NotFoundError,
-            self.sc.create_writer,
-            self.invalidStream)
-
-
-    def test_stream_writer_successful_writing(self):
+        # Write to this stream
         sw = self.sc.create_writer(self.validStream)
 
         def on_response(response):
@@ -176,3 +97,24 @@ class StreamTestBase(object):
         q = sw.write(self.messageToWrite)
         q.on_response(on_response)
         q.on_response(check_exit_code, check_exit_code)
+
+    def test_set_ttl_invalid_stream(self):
+        ttl = 88888
+
+        self.assertRaises(
+            NotFoundError,
+            self.sc.set_ttl,
+            self.invalidStream,
+            ttl
+        )
+
+        self.assertRaises(
+            NotFoundError,
+            self.sc.get_ttl,
+            self.invalidStream
+        )
+
+        self.assertRaises(
+            NotFoundError,
+            self.sc.create_writer,
+            self.invalidStream)
