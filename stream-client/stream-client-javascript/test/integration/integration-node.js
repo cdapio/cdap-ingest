bd8076cb48d2eee21d97cdcd1a7e1a2aba7285ef
Constantine Povietkin <kpovetkin@continuuity.com>
2014-10-20 18:03:57 +0300

    [js_ingest] integration tests added

---
+++ stream-client/stream-client-javascript/test/integration/integration-node.js
@@ -0,0 +1,278 @@
+var expect = require('expect.js'),
+    StreamClient = require('cdap-stream-client'),
+    AuthManager = require('cdap-auth-client'),
+
+    /**
+     * Server connection info
+     */
+    host = '107.178.222.50',
+    port = '10000',
+    ssl = false,
+    user = 'kpovetkin',
+    pass = 'RhtcE808',
+    authManager = new AuthManager();
+
+authManager.setConnectionInfo(host, port, ssl);
+authManager.configure({
+    username: user,
+    password: pass
+});
+
+console.log('Authentication info');
+console.log('Enabled: ', authManager.isAuthEnabled());
+console.log('Token: ', authManager.getToken());
+
+describe('CDAP ingest tests', function () {
+    describe('StreamClient object creation', function () {
+        it('Constructor creates an object', function () {
+            var streamClient = new StreamClient({
+                host: host,
+                port: port
+            });
+
+            expect(streamClient).to.be.an('object');
+        });
+
+        it('Constructor creates a valid object', function () {
+            var streamClient = new StreamClient({
+                host: host,
+                port: port
+            });
+
+            expect(streamClient).to.have.property('create');
+            expect(streamClient).to.have.property('setTTL');
+            expect(streamClient).to.have.property('getTTL');
+            expect(streamClient).to.have.property('truncate');
+            expect(streamClient).to.have.property('createWriter');
+        });
+
+        it('Constructor could not be called without "config" object', function () {
+            expect(function () {
+                var streamClient = new StreamClient();
+            }).to.throwError();
+        });
+
+        it('"config" object for the constructor should contain "host" field', function () {
+            expect(function () {
+                var streamClient = new StreamClient({port: port});
+            }).to.throwError();
+        });
+
+        it('"config" object for the constructor should contain "port" field', function () {
+            expect(function () {
+                var streamClient = new StreamClient({host: host});
+            }).to.throwError();
+        });
+    });
+
+    describe('Functionality', function () {
+        it('"setTTL" for a valid stream', function () {
+            var streamName = 'newStream',
+                ttl = 86400,
+                streamClient = new StreamClient({
+                    host: host,
+                    port: port,
+                    ssl: ssl,
+                    authManager: authManager
+                });
+            streamClient.create(streamName);
+            streamClient.setTTL(streamName, ttl);
+        });
+
+        it('"setTTL" for an invalid stream', function () {
+            var streamName = 'invalidStream',
+                ttl = 86400,
+                streamClient = new StreamClient({
+                    host: host,
+                    port: port,
+                    ssl: ssl,
+                    authManager: authManager
+                });
+
+            expect(function () {
+                streamClient.setTTL(streamName, ttl);
+            }).to.throwError();
+        });
+
+        it('"getTTL" for a valid stream and a valid TTL value', function () {
+            var streamName = 'newStream',
+                ttl = 86400,
+                streamClient = new StreamClient({
+                    host: host,
+                    port: port,
+                    ssl: ssl,
+                    authManager: authManager
+                });
+
+            var respTTL = streamClient.getTTL(streamName);
+
+            expect(respTTL).to.be.equal(ttl);
+        });
+
+        it('"getTTL" for a valid stream and a wrong TTL value', function () {
+            var streamName = 'newStream',
+                invalidTTL = -1,
+                streamClient = new StreamClient({
+                    host: host,
+                    port: port,
+                    ssl: ssl,
+                    authManager: authManager
+                });
+
+            streamClient.create(streamName);
+            var respTTL = streamClient.getTTL(streamName);
+
+            expect(respTTL).not.to.be.equal(invalidTTL);
+        });
+
+        it('"getTTL" for an invalid stream', function () {
+            var streamName = 'invalidStream',
+                ttl = 86400,
+
+                streamClient = new StreamClient({
+                    host: host,
+                    port: port,
+                    ssl: ssl,
+                    authManager: authManager
+                });
+
+            expect(function () {
+                streamClient.getTTL(streamName);
+            }).to.throwError();
+        });
+
+        it('"truncate" for a valid stream', function () {
+            var streamName = 'newStream',
+                streamClient = new StreamClient({
+                    host: host,
+                    port: port,
+                    ssl: ssl,
+                    authManager: authManager
+                });
+
+            streamClient.create(streamName);
+            streamClient.truncate(streamName);
+        });
+
+        it('"truncate" for an invalid stream', function () {
+            var streamName = 'invalidStream',
+                streamClient = new StreamClient({
+                    host: host,
+                    port: port,
+                    ssl: ssl,
+                    authManager: authManager
+                });
+
+            expect(function () {
+                streamClient.truncate(streamName);
+            }).to.throwError();
+        });
+
+        it('"createWriter" creates a valid object', function () {
+            var streamName = 'newStream',
+                streamClient = new StreamClient({
+                    host: host,
+                    port: port,
+                    ssl: ssl,
+                    authManager: authManager
+                });
+
+            var streamWriter = streamClient.createWriter(streamName);
+
+            expect(streamWriter).to.have.property('write');
+        });
+
+        describe('StreamWriter', function () {
+            it('"write" returns valid Promise object', function () {
+                var streamName = 'newStream',
+                    textToSend = 'klasj ddkjas ldjas kljfasklj fklasfj a',
+                    streamClient = new StreamClient({
+                        host: host,
+                        port: port,
+                        ssl: ssl,
+                        authManager: authManager
+                    });
+
+                var streamWriter = streamClient.createWriter(streamName),
+                    promise = streamWriter.write(textToSend);
+
+                expect(promise).to.have.property('then');
+                expect(promise).to.have.property('catch');
+                expect(promise).to.have.property('notify');
+                expect(promise).to.have.property('reject');
+                expect(promise).to.have.property('resolve');
+            });
+
+            describe('Promise states', function () {
+                it('"resolve" fires a handler', function () {
+                    var streamName = 'newStream',
+                        textToSend = 'klasj ddkjas ldjas kljfasklj fklasfj a',
+                        streamClient = new StreamClient({
+                            host: host,
+                            port: port,
+                            ssl: ssl,
+                            authManager: authManager
+                        });
+
+                    var streamWriter = streamClient.createWriter(streamName),
+                        promise = streamWriter.write(textToSend),
+                        resolved = false,
+                        promiseHandler = function (status) {
+                            resolved = true;
+                        },
+                        promiseChecker = function () {
+                            expect(resolved).to.be.ok();
+                        };
+
+                    promise.then(promiseHandler).then(promiseChecker, promiseChecker);
+                });
+
+                it('"catch" fires a handler', function () {
+                    var streamName = 'newStream',
+                        textToSend = 'klasj ddkjas ldjas kljfasklj fklasfj a',
+                        streamClient = new StreamClient({
+                            host: host,
+                            port: port,
+                            ssl: ssl,
+                            authManager: authManager
+                        });
+
+                    var streamWriter = streamClient.createWriter(streamName),
+                        promise = streamWriter.write(textToSend),
+                        resolved = false,
+                        promiseHandler = function (status) {
+                            resolved = true;
+                        },
+                        promiseChecker = function () {
+                            expect(resolved).to.be.ok();
+                        };
+
+                    promise.catch(promiseHandler).then(promiseChecker, promiseChecker);
+                });
+
+                it('"notify" fires a handler', function () {
+                    var streamName = 'newStream',
+                        textToSend = 'klasj ddkjas ldjas kljfasklj fklasfj a',
+                        streamClient = new StreamClient({
+                            host: host,
+                            port: port,
+                            ssl: ssl,
+                            authManager: authManager
+                        });
+
+                    var streamWriter = streamClient.createWriter(streamName),
+                        promise = streamWriter.write(textToSend),
+                        resolved = false,
+                        promiseHandler = function (status) {
+                            resolved = true;
+                        },
+                        promiseChecker = function () {
+                            expect(resolved).to.be.ok();
+                        };
+
+                    promise.then(null, null, promiseHandler).then(promiseChecker, promiseChecker);
+                });
+            });
+        });
+    });
+});
