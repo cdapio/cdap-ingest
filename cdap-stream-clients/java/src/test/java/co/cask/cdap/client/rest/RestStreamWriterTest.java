7ab8340e0a3366183887f06935be8fac5c296cc6
anew <andreas@continuuity.com>
2015-04-17 10:11:35 -0700

    update copyrights that were not updated in last PR

--- cdap-stream-clients/java/src/test/java/co/cask/cdap/client/rest/RestStreamWriterTest.java
+++ cdap-stream-clients/java/src/test/java/co/cask/cdap/client/rest/RestStreamWriterTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2014 Cask Data, Inc.
+ * Copyright © 2014-2015 Cask Data, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -37,7 +37,7 @@ import java.util.concurrent.ExecutionException;
 import static org.junit.Assert.assertEquals;
 
 /**
- * Unit tests for the {@link co.cask.cdap.client.rest.RestStreamWriter} class.
+ * Unit test for the {@link co.cask.cdap.client.rest.RestStreamWriter} class.
  */
 public abstract class RestStreamWriterTest extends RestTest {
 
